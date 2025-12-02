539f091394da056a2ca71f6f29136900f4c3ec57
KerelosNasser <157994690+KerelosNasser@users.noreply.github.com>
2025-12-02 22:04:49 +1000

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './sections/navbar'
+import Hero from './sections/Hero'
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-4xl">
+
+      <Navbar/>
+      <Hero/>
+      {/*about*/}
+      {/*projects*/}
+      {/* experience */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  )
+}
+
+export default App
