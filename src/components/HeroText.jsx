dd6e51ae2beb0fcf0ed6fd1008a5d5cb478aa644
KerelosNasser <157994690+KerelosNasser@users.noreply.github.com>
2025-12-06 15:28:46 +1000

    Refactor FlipWords component and update HeroText to include dynamic word animation

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -6,23 +6,31 @@ function HeroText() {
     <div className='z-10 mt-20 text-center md:text-left md:mt-40 rounded-r-3xl bg-clip-text'>
       {/* desktop */}
       <div className='flex-col hidden md:flex c-space'>
-        <h1>
+        <h1 className='text-3xl mb-4 font-medium text-neutral-300'>
           Hi there, I'm
           <span className='text-5xl font-bold text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text'>
             Kerelos
           </span>
         </h1>
-        <div className="flex flex-col items-start">
-          <p className="text-5xl font-medium text-neutral-300">
-            A Developer Dedicated to Crafting</p>
-            {/* flipword.jsx */}
-           <div> <FlipWords/> </div>
-          <p className="text-5xl font-medium text-neutral-300">Web Solutions
+        <div className='flex flex-col items-start'>
+          <p className='text-5xl font-medium text-neutral-300'>
+            A Developer Dedicated to Crafting
           </p>
+          {/* flipword.jsx */}
+          <div>
+            <FlipWords
+              words={["experiences", "innovation", "Scallable"]}
+              className="font-black text-white text-6xl"
+              duration={1500}
+            />
+          </div>
+          <p className='text-5xl font-medium text-neutral-300'>Web Solutions</p>
         </div>
       </div>
       {/* mobile */}
-      <div></div>
+      <div className="">
+
+      </div>
     </div>
   );
 }
