4cccc2d3f029d1e7f46f063c10176164a7cf27d5
KerelosNasser <157994690+KerelosNasser@users.noreply.github.com>
2025-12-06 16:35:51 +0200

    modified:   src/components/ParallelBackground.jsx

--- src/components/ParallelBackground.jsx
+++ src/components/ParallelBackground.jsx
@@ -1,62 +1,69 @@
-import { motion , useScroll, useTransform } from "motion/react";
+import { motion, useScroll, useTransform } from "motion/react";
+
 function ParallelBackground() {
-  const {ScrollYProgress} = useScroll();
-  const mountain3y = useTransform(ScrollYProgress, [0,0.5], ['0%','70%']);
-  const mountain2y = useTransform(ScrollYProgress, [0,0.5], ['0%','30%']);
-  const mountain1y = useTransform(ScrollYProgress, [0,0.5], ['0%','0%']);
-  const planet = useTransform(ScrollYProgress, [0,0.5], ['0%','-20%']);
+  const { scrollYProgress } = useScroll();
+
+  const mountain3y = useTransform(scrollYProgress, [0, 0.5], ["0%", "70%"]);
+  const mountain2y = useTransform(scrollYProgress, [0, 0.5], ["0%", "30%"]);
+  const mountain1y = useTransform(scrollYProgress, [0, 0.5], ["0%", "0%"]);
+  const planet = useTransform(scrollYProgress, [0, 0.5], ["0%", "-20%"]);
 
   return (
-    <section className='absolute inset-0 bg-black/40'>
-      <div className=' relative h-screen overflow-y-hidden'>
-            {/* skybackground */}
-        <div className="absolute inset-0 h-screen w-full -z-50"
-        style={{backgroundImage:"url(/assets/sky.jpg)",
-          backgroundPosition: "bottom",
-          backgroundSize: "cover",
+    <section className="absolute inset-0 bg-black/40">
+      <div className="relative h-screen overflow-y-hidden">
+        {/* skybackground */}
+        <div
+          className="absolute inset-0 h-screen w-full -z-50"
+          style={{
+            backgroundImage: "url(/assets/sky.jpg)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+          }}
+        />
 
-        }}
-        >
-
-        </div>
         {/* layer 3 mountain */}
-        <motion.div className="absolute inset-0 -z-40"
-        style={{backgroundImage:"url(/assets/mountain-3.png)",
-          backgroundPosition: "bottom",
-          backgroundSize: "cover",
-          y: mountain3y,
+        <motion.div
+          className="absolute inset-0 -z-40"
+          style={{
+            backgroundImage: "url(/assets/mountain-3.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: mountain3y,
+          }}
+        />
 
-        }}
-        >
-        </motion.div>
         {/* planets */}
-        <motion.div className="absolute inset-0 -z-30"
-        style={{backgroundImage:"url(/assets/planets.png)",
-          backgroundPosition: "bottom",
-          backgroundSize: "cover",
-          x : planet,
+        <motion.div
+          className="absolute inset-0 -z-30"
+          style={{
+            backgroundImage: "url(/assets/planets.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            x: planet,
+          }}
+        />
 
-        }}
-        >
+        {/* layer 2 mountain */}
+        <motion.div
+          className="absolute inset-0 -z-20"
+          style={{
+            backgroundImage: "url(/assets/mountain-2.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: mountain2y,
+          }}
+        />
 
-        </motion.div>
-        {/* layer 2 mountatin */}
-        <motion.div className="absolute inset-0 -z-20"
-        style={{backgroundImage:"url(/assets/mountain-2.png)",
-          backgroundPosition: "bottom",
-          backgroundSize: "cover",
-          y: mountain2y,
-
-        }}
-        ></motion.div>
         {/* layer 1 mountain */}
-        <motion.div className="absolute inset-0 -z-10"
-        style={{backgroundImage:"url(/assets/mountain-1.png)",
-          backgroundPosition: "bottom",
-          backgroundSize: "cover",
-          y: mountain1y,
-        }}
-        ></motion.div>
+        <motion.div
+          className="absolute inset-0 -z-10"
+          style={{
+            backgroundImage: "url(/assets/mountain-1.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: mountain1y,
+          }}
+        />
       </div>
     </section>
   );
